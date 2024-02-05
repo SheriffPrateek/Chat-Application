4fa6b18153ce184f18cf6bf01d0d5720b13aa38c
SheriffPrateek <prateeksaxena311@gmail.com>
2024-02-05 13:35:55 +0530

    Project Completed

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const express=require('express');
+    const express=require('express');
+    const app=express();
+    const Server=require('socket.io');
+    const http=require('http');
+    const server=http.createServer(app);
+    const io=Server(server);             
+    app.set('view engine','ejs');
 
-const app=express();
-const Server=require('socket.io');
-const http=require('http');
+    app.get('/',(req,resp)=>{
+    
+        resp.render('Home');
+    });
 
-const server=http.createServer(app);
-const io=Server(server);
-app.set('view engine','ejs');
+    io.on("connection",(socket)=>{       
+        socket.on('chat',(obj)=>{
+            socket.broadcast.emit('message',obj);
+        })
+    });
 
-app.get('/',(req,resp)=>{
-    resp.render('Example');
-});
-
-//Socket se related work handle here
-io.on("connection",(socket)=>{       // io reprsents all sockets n clients
-  //  console.log(socket.id);
-    socket.on('chat',(input)=>{
-        socket.broadcast.emit('message',input);
-    })
-});
-
-server.listen(5000);
+    server.listen(5000);
